da739f2023437bde3a22db5550dd76847091a3a1
vincentcombet <vincent.combet@soprasteria.com>
2020-02-24 10:26:41 +0100

    2 files modified

--- src/app/events-manager/events-manager.component.ts
+++ src/app/events-manager/events-manager.component.ts
@@ -8,10 +8,20 @@ import { events } from '../events-list';
   styleUrls: ['./events-manager.component.css']
 })
 export class EventsManagerComponent implements OnInit {
+  currentEvent = "none";
+  d3 = 0;
+
+  events = events;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  generateEvent() {
+    this.d3 = Math.floor((Math.random() * 3) + 1);
+
+    this.currentEvent = events[this.d3].desc;
   }
 
 }
