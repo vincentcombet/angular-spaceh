1cbc75513fd45e942b97acc985fda672ccdeed63
vincentcombet <vincent.combet@soprasteria.com>
2020-02-25 14:00:45 +0100

    2 files modified

--- src/app/data/events-list.ts
+++ src/app/data/events-list.ts
@@ -1,74 +1,74 @@
 export const events = [
   {
     id: 11,
-    desc: '11'
+    desc: '<b>La mort vient d\'en haut (4)</b><br />Générez un Genestealer sur les cases adjacentes à chaque Space Marine qui n\'est pas déjà engagé avec un Genestealer. Suivre cette priorité de placement: derrière un Space Marine, sur les flancs ou bien devant.'
   },
   {
     id: 12,
-    desc: '12'
+    desc: '<b>L\'essaim (4)</b><br />Générez deux blips à 4 cases derrière un Space Marine. Selon la cible prioritaire de la mission.'
   },
   {
     id: 13,
-    desc: '13'
+    desc: '<b>Concentration</b><br />Choisissez un Space Marine, il pourra relancer un jet de dés durant ce tour.'
   },
   {
     id: 14,
-    desc: '14'
+    desc: '<b>Foi divine</b><br />Choisissez un Genestealer et lancé un dé. Sur 5+ le Genestealer est éliminé.'
   },
   {
     id: 15,
-    desc: '15'
+    desc: '<b>Jet de vapeur (3)</b><br />Les blips à plus de 4 cases peuvent se déplacer dans la LDV des Space Marines sans être révélés. Les Genestealers à plus de 4 cases sont touchés que sur 6 y compris en tir soutenu.'
   },
   {
     id: 16,
-    desc: '16'
+    desc: '<b>Ils arrivent ! (5)</b><br />Convertissez 3 blips proche d\'un Space Marine selon la cible prioritaire de la mission.'
   },
   {
     id: 21,
-    desc: '21'
+    desc: '<b>Trou dans le sol</b><br />Placez un trou sur la cible de la mission. Ce Space Marine doit dépenser 2 PA, venant de ses actions et non Commandement, pour le relever pendant son tour. Ce trou est un point d\'entrée Genestealer (limité à 2 trous par partie).'
   },
   {
     id: 22,
-    desc: '22'
+    desc: '<b>Oppression (3)</b><br />Générez deux Genestealers derrière un Space Marine. Selon la cible prioritaire de la mission.'
   },
   {
     id: 23,
-    desc: '23'
+    desc: '<b>Embuscade (4)</b><br />Générez un blip derrière un Space Marine. Selon la cible prioritaire de la mission.'
   },
   {
     id: 24,
-    desc: '24'
+    desc: '<b>Arme enrayée (3)</b><br />Un Space Marine ne pourra pas utiliser son arme, ni un état d\'alerte. Selon la cible prioritaire de la mission.'
   },
   {
     id: 25,
-    desc: '25'
+    desc: '<b>Attaque sournoise (2)</b><br />Un Genestealer vous aggripe au travers d\'un mur ! Défendez-vous comme une attaque standard, sur un succès le Genestealer s\'en va, sur un échec il vous bloque et vous perdez votre prochain tour. Selon la cible prioritaire de la mission.'
   },
   {
     id: 26,
-    desc: '26'
+    desc: '<b>Attaque psychique</b><br />La cible de la mission subit une attaque psychique et perds 2 PA. Lancez 1 dé, sur un 5+ l\'attaque échoue.'
   },
   {
     id: 31,
-    desc: '31'
+    desc: '<b>Collision de météorites (3)</b><br />Le Hulk devient instable, tous les déplacements sont réduits de 1. L\'état de mise en alerte coûte 3 PA pour le prochain tour.'
   },
   {
     id: 32,
-    desc: '32'
+    desc: '<b>Réapprovisionnement</b><br />Obtenez +2 PA lors de votre prochaine pioche de points de commandement, maximum 6.'
   },
   {
     id: 33,
-    desc: '33'
+    desc: '<b>Court-circuit (3)</b><br />Impossible pour un Space Marine d\'utiliser l\'action de verrouillage de porte ce tour.'
   },
   {
     id: 34,
-    desc: '34'
+    desc: '<b>Teamwork</b><br />Choisissez un Space Marine adjacent à un autre, il bénéficie d\'une relance gratuite par tir non soutenu.'
   },
   {
     id: 35,
-    desc: '35'
+    desc: '<b>J\ai des signaux partout ! (6)</b><br />Tournez chaque Space Marine afin qu\ils soient tous dos aux blips et Genestealers.'
   },
   {
     id: 36,
-    desc: '36'
+    desc: '<b>Brouillage radio (2)</b><br />Divisez les points de commandement par 2 durant le prochain tour, arrondi au supérieur.'
   }
 ];
