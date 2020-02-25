c8409d4b1e91895b67ee139e0f8ea542d808415d
vincentcombet <vincent.combet@soprasteria.com>
2020-02-25 09:39:39 +0100

    1 file modified

--- src/app/exploration-manager/exploration-manager.component.ts
+++ src/app/exploration-manager/exploration-manager.component.ts
@@ -13,12 +13,7 @@ import { rooms } from '../data/rooms-list';
 export class ExplorationManagerComponent implements OnInit {
   currentCorridor = "none";
   currentRoom = "none";
-  explorationD12 = 0;
-  corridorContentD6 = 0;
-  corridorEndD6 = 0;
-  roomD6 = 0;
-  nbDiscoveredRooms = 0
-  value = 0;
+  nbDiscoveredRooms = 0;
 
   explorations = explorations;
 
@@ -28,9 +23,9 @@ export class ExplorationManagerComponent implements OnInit {
   }
 
   explorate() {
-    this.explorationD12 = 9;//Math.floor((Math.random() * 11) + 2);
+    var explorationD12 = Math.floor((Math.random() * 11) + 2);
 
-    var explorationResult = explorations.find(x => x.id == this.explorationD12).desc;
+    var explorationResult = explorations.find(x => x.id == explorationD12).desc;
 
     if (explorationResult == "room") {
       this.currentRoom = this.generateRoom();
@@ -41,11 +36,11 @@ export class ExplorationManagerComponent implements OnInit {
         this.currentRoom = this.generateRoom();
       } else if (explorationResult.indexOf("relaunch") != -1) {
         // FIXME
-        this.explorationD12 = Math.floor((Math.random() * 11) + 2);
-        while (this.explorationD12 == 7) {
-          this.explorationD12 = Math.floor((Math.random() * 11) + 2);
+        var explorationD12 = Math.floor((Math.random() * 11) + 2);
+        while (explorationD12 == 7) {
+          explorationD12 = Math.floor((Math.random() * 11) + 2);
         }
-        var explorationResult2 = explorations.find(x => x.id == this.explorationD12).desc;
+        var explorationResult2 = explorations.find(x => x.id == explorationD12).desc;
         this.currentCorridor = this.currentCorridor.replace("relaunch", "(" + explorationResult2 + ")");
       } else {
         this.currentRoom = "none";
@@ -54,27 +49,27 @@ export class ExplorationManagerComponent implements OnInit {
   }
 
   generateRoom() {
-    this.roomD6 = Math.floor((Math.random() * 6) + 1);
-      var indexRoom = this.roomD6 + this.nbDiscoveredRooms;
-      if (indexRoom > 8) {
-        indexRoom = 8;
-      }
-      var tempRoom = rooms.find(x => x.id == indexRoom).desc;
-      this.nbDiscoveredRooms++;
+    var roomD6 = Math.floor((Math.random() * 6) + 1);
+    var indexRoom = roomD6 + this.nbDiscoveredRooms;
+    if (indexRoom > 8) {
+      indexRoom = 8;
+    }
+    var tempRoom = rooms.find(x => x.id == indexRoom).desc;
+    this.nbDiscoveredRooms++;
 
-      return tempRoom;
+    return tempRoom;
   }
 
   generateCorridor() {
-    this.corridorContentD6 = Math.floor((Math.random() * 6) + 1);
-      var content = corridorContents.find(x => x.id == this.corridorContentD6).desc;
+    var corridorContentD6 = Math.floor((Math.random() * 6) + 1);
+    var content = corridorContents.find(x => x.id == corridorContentD6).desc;
 
-      this.corridorEndD6 = Math.floor((Math.random() * 6) + 1);
-      var end = corridorEnds.find(x => x.id == this.corridorEndD6).desc;
+    var corridorEndD6 = Math.floor((Math.random() * 6) + 1);
+    var end = corridorEnds.find(x => x.id == corridorEndD6).desc;
 
-      var tempCorridor = " (content: " + content + ", end: " + end + ")";
+    var tempCorridor = " (content: " + content + ", end: " + end + ")";
 
-      return tempCorridor;
-      
+    return tempCorridor;
+
   }
 }
